796ad6ece51af511618b581ec00023e3f5fc9459
Suyogrg <123380700+Suyogrg@users.noreply.github.com>
2026-08-10 15:44:16 +0100

    temporary working html form

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
-@app.route('/')
+def load_items():
+    with open("items.json", "r") as file:
+        data = json.load(file)
+
+    return data
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+
+        credits = int(request.form["credits"])
+        selected_agent = request.form["agent"]
+
+        items = load_items()
+
+        filtered_items = []
+
+        for item in items:
+            if item.get("agent") == selected_agent or item.get("agent") == None:
+                if item.get("cost") <= credits:
+                    filtered_items.append(item)
+
+        return render_template(
+            "index.html",
+            credits=credits,
+            selected_agent=selected_agent,
+            items=filtered_items
+        )
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=False)
